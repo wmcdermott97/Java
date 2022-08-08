cf2b57fda794e0a324d313da7f68dc0eb5676f0e
William McDermott <wmcdermott.math@gmail.com>
2022-08-08 08:59:40 -0400

    Making sure things run properly before tests

---
+++ StaticBinaryTree/Init.java
@@ -0,0 +1,8 @@
+package StaticBinaryTree;
+
+public class Init {
+    public static void main(String[] args) {
+        System.out.println("Here the main things happen with the tree");
+        // Initialize a tree with some test values, perform operations on it, etc.
+    }
+}
