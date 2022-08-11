5b3c6808edb736bc89474b23a66252cbea9a22bf
William McDermott <wmcdermott.math@gmail.com>
2022-08-11 18:11:33 -0400

    Working on test suite at abstract level

--- BinaryTree/SortedListTest.java
+++ BinaryTree/SortedListTest.java
@@ -4,19 +4,53 @@ package BinaryTree;
  *  Test class for the SortedList Interface, filled with wonderful unit tests.
  */
 public class SortedListTest {
-
     /**
      * The SortedList to test on. Will be reset between runs.
      * 
-     * I chose to use Integers for simplicity, but it may be wise to allow for more flexibility.
+     * I chose to use Integers for simplicity, but it may be wise to try different types.
      */
-    private SortedList<String> list;
+    private SortedList<Integer> list;
     
     /**
-     * Construct a new SortedListTest with the appropriate fields.
+     * The number of tests in this class, because I may modify the code somehow to be less annoying looking.
      */
-    public SortedListTest() {
+    private final int NUM_TESTS = 0;
 
+    /**
+     * Construct a new SortedListTest with the appropriate fields.
+     * 
+     * @param list The SortedList instance to test on, dependent on implementation.
+     */ 
+    public SortedListTest(SortedList<Integer> list) {
+        this.list = list;
+    }
+
+    /**
+     * Run all Tests, printing results to the console.
+     * 
+     * @return Number of tests that passed, which is the 
+     */
+    public boolean getTestReport() {
+        boolean target = true;
+        for (int testI = 0; testI < NUM_TESTS; testI++) {
+            System.out.println("Running test", testI);
+            target = target && this.getTestRun(testI, true);
+            // Running a test modifies the output, so we'll need to reset each time.
+            this.reset();
+        }
+        System.out.println("All tests passed: ", target);
+        return target;
+    }
+
+    /**
+     * Runs a specific test by its integer id.
+     * 
+     * @param id The test to run in the sequence.
+     * @param verbose Whether or not to print the results of the test individually.
+     */
+    public boolean getTestRun(int id, boolean verbose) {
+        // TODO
+        return false;
     }
 
     /**
@@ -24,8 +58,9 @@ public class SortedListTest {
      * 
      * I could be wrong, but if there is a spoof for this, then that messes with the code.
      */
-    public void reset() {
-
+    private void reset() {
+        // Anything else that may need to happen.
+        list.clear();
     }
 
 }
